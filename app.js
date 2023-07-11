a69f005e2993e6398a9b08af29ebc26966f44834
Kia Boluki <kia.boluki@gmail.com>
2023-07-11 19:10:05 +0400

    Operators , Priorities

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('من از فایل اپ جی اس می آیم')
+// console.log( 5 + 8)
+// console.log( 5 * 8)
+// console.log( 5 / 8)
+// console.log( 8 % 5)
+// console.log( 9 % 5)
+// console.log( 10 % 5)
+// console.log( 11 % 5)
+// console.log( 12 % 5)
+// console.log( 13 % 5)
+
+// console.log( 2 * 75000 + 1 * 15000 )
+
+console.log( ( 2 + 8 ) * 2 / 5)
