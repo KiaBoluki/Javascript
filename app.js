724b4cb13769c5d8b66735d2c3e14b1397ab5aa6
Kia Boluki <kia.boluki@gmail.com>
2023-07-13 15:51:31 +0400

    click event

--- app.js
+++ app.js
@@ -10,4 +10,17 @@
 
 // console.log( 2 * 75000 + 1 * 15000 )
 
-console.log( ( 2 + 8 ) * 2 / 5)
+// console.log( ( 2 + 8 ) * 2 / 5)
+
+// => console.log ( tedad * mablagh );
+
+
+const tedad = 4
+
+const mablagh = 75000
+
+// console.log(tedad * mablagh)
+
+const btnMohasebe = document.querySelector("#btn");
+
+btnMohasebe.addEventListener('click', () => document.write('Ghorbanat'))
