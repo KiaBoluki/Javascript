05b8314136827e65946af06aad1fb12679445a11
Kia Boluki <kia.boluki@gmail.com>
2023-07-27 23:00:37 +0400

    if else statement

--- football.js
+++ football.js
@@ -1,23 +1,19 @@
-const inputTeamName = document.querySelector('#team-name');
-const btnSubmit = document.querySelector('#btn-submit');
-const teamLogo = document.querySelector('#team-logo'); 
+const inputTeamName = document.querySelector("#team-name");
+const btnSubmit = document.querySelector("#btn-submit");
+const teamLogo = document.querySelector("#team-logo");
 
-btnSubmit.addEventListener('click', () => {
-    const teamName = inputTeamName.value;
+btnSubmit.addEventListener("click", () => {
+  const teamName = inputTeamName.value;
 
-    if ( teamName == 'barca' ) {
-        // console.log( teamName + " is a good team")
-        teamLogo.src = "images/barca.png"
-    } else 
-    if ( teamName == 'real' ) {
-        // console.log( teamName + " is a good team")
-        teamLogo.src = "images/real.webp"
-    }
-    else if ( teamName == 'liverpool'){
-        teamLogo.src = 'images/liverpool.png';
-    }
-    
-    else {
-        window.alert(teamName + " is not in our list.");
-    }
-})
+  if (teamName == "barca") {
+    // console.log( teamName + " is a good team")
+    teamLogo.src = "images/barca.png";
+  } else if (teamName == "real") {
+    // console.log( teamName + " is a good team")
+    teamLogo.src = "images/real.webp";
+  } else if (teamName == "liverpool") {
+    teamLogo.src = "images/liverpool.png";
+  } else {
+    window.alert(teamName + " is not in our list.");
+  }
+});
