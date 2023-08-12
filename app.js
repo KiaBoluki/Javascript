9d5ed31dfe5f08af2e00b85a44fe6b61af4b0cac
Kia Boluki <kia.boluki@gmail.com>
2023-08-12 18:58:30 +0400

    Assignments

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
-for ( let i = 0; i < 100; i++ )
-{
-    console.log(i);
-}
+let a = 0; 
+let b = 0; 
+let c = 0; 
+
+
+a += 1 ; 
+b++;
+c += 55; // c = c + 55 ; 
+
+c -= 5; // c = c - 5 ; 
+c *= 2; // c = c * 2 ; 
+
+// logs
+console.log('a:', a ); 
+console.log('b:', b);
+console.log('c:', c);
