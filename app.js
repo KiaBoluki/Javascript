699cc22a38f50a11385d8024931fe5bf92e16c68
Kia Boluki <kia.boluki@gmail.com>
2023-08-12 18:40:27 +0400

    for loop

--- app.js
+++ app.js
@@ -1,29 +1,4 @@
-const number1 = 5;
-const number2 = 6;
-
-if (number1 > 0) {
-  /*
-    NOT GLOBAL SCOPE 
-    */
-  // number3++;
-
-  let number3 = 8;
-    number3 ++;
-
-
-
-  console.log(number3);
+for ( let i = 0; i < 100; i++ )
+{
+    console.log(i);
 }
-
-if (number1 > 1) {
-  /*
-    NOT GLOBAL SCOPE 
-    */
-  console.log(number3);
-}
-
-/*
-
-GLOBAL SCOPE 
-بخش عمومی 
-*/
